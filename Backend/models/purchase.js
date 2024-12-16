07aa66fa368c57a18a36478a82928b8d5f255723
Alwin Denny <alwin.009@gmail.com>
2024-12-16 11:23:48 +0530

    changes

--- Backend/models/purchase.js
+++ Backend/models/purchase.js
@@ -1,31 +1,31 @@
 const mongoose = require("mongoose");
 
 const PurchaseSchema = new mongoose.Schema(
-  {
-    userID: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "users",
-      required: true,
+    {
+        userID: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "users",
+            required: true,
+        },
+        ProductID: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "product",
+            required: true,
+        },
+        QuantityPurchased: {
+            type: Number,
+            required: true,
+        },
+        PurchaseDate: {
+            type: String,
+            required: true,
+        },
+        TotalPurchaseAmount: {
+            type: Number,
+            required: true,
+        },
     },
-    ProductID: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "product",
-      required: true,
-    },
-    QuantityPurchased: {
-      type: Number,
-      required: true,
-    },
-    PurchaseDate: {
-      type: String,
-      required: true,
-    },
-    TotalPurchaseAmount: {
-      type: Number,
-      required: true,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 const Purchase = mongoose.model("purchase", PurchaseSchema);
